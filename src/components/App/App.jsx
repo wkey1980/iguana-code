b5ae17d6e1a0fee306577778ec7ef3f20e010e8f
Will <28814921+wkey1980@users.noreply.github.com>
2021-07-18 01:31:32 +0100

    Setup project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+// Global Styles
+import GlobalStyle from '../../styles/index'
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>App Hello World!</h1>
+
+    </div>
+  );
+}
+
+export default App;
