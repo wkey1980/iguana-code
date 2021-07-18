329e741177704a007a769e559a4c050971bbef18
Will <28814921+wkey1980@users.noreply.github.com>
2021-07-18 02:02:25 +0100

    Starting to build the Navbar component and navigation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 // Global Styles
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import GlobalStyle from '../../styles/index'
+import Navbar from '../Navbar/index'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <h1>App Hello World!</h1>
-
+        <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components/macro";
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <Nav>
+            <h1>Navbar Component</h1>
+            <h3>Second</h3>
+        </Nav>
+    )
+}
+const Nav = styled.div``;
+
+
+
+export default Navbar
