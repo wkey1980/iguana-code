2e782353a139143abf791c14a22fe83ad0304378
Will <28814921+wkey1980@users.noreply.github.com>
2021-08-01 21:25:24 +0100

    Started to create the contact form

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@material-ui/core'
+import React from 'react'
+
+const ContactForm = () => {
+    return (
+        <div>
+            <h1>Contact Form</h1>
+            <Button variant="contained">Submit</Button>
+        </div>
+    )
+}
+
+export default ContactForm
